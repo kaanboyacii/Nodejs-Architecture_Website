13e886ad7e3f9c2738abd3cd35263164d908c4b2
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-08-20 10:58:01 +0300

    Projects page completed

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -1,9 +1,9 @@
 exports.getIndex = (req, res, next) => {
-    res.render('index',{title: 'HomePage'});
+    res.render('index',{title: 'İz Mimarlık'});
 }
 exports.getAboutPage = (req, res, next) => {
-    res.render('about',{title: 'About Page'});
+    res.render('about',{title: 'Hakkımızda'});
 }
 exports.getProjectPage = (req, res, next) => {
-    res.render('about',{title: 'About Page'});
+    res.render('projects',{title: 'Projelerimiz'});
 }
--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const homeController = require('../controllers/homecontroller');
 
 router.get('/anasayfa',homeController.getIndex);
-router.get('/hakkımızda',homeController.getAboutPage);
+router.get('/hakkimizda',homeController.getAboutPage);
 router.get('/projelerimiz',homeController.getProjectPage);
 
 module.exports = router;
