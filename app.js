ec3aa820533657c016fb9c483990fce758b582fd
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-08-21 01:14:43 +0300

    Error page

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ app.set('view engine','pug');
 app.set('views',path.join(__dirname,'views'));
 
 const Routes = require('./routes/routes')
-
+const errorController = require('./controllers/errorcontroller');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
 
 //Routes
 app.use(Routes);
+app.use(errorController.get404Page);
 
 app.listen(3000, ()=> {
     console.log('listening on port 3000')
