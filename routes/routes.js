7b7f249b349590ac0c872ae8811437ec655fce7c
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-08-22 09:44:17 +0300

    Routes & css fix

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const homeController = require('../controllers/homecontroller');
 
+router.get('/',homeController.getIndex);
 router.get('/anasayfa',homeController.getIndex);
 router.get('/hakkimizda',homeController.getAboutPage);
 router.get('/projelerimiz',homeController.getProjectPage);
 router.get('/iletisim',homeController.getContactPage);
 router.get('/servislerimiz',homeController.getServicesPage);
+
 
 module.exports = router;
